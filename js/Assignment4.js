5ee26a82239569785d0c85c7ad021372424d4684
kfilby <kfilby@uab.edu>
2017-03-06 21:26:49 -0600

    Changes to the js
    Removed the else statement since it was uneeded

--- js/Assignment4.js
+++ js/Assignment4.js
@@ -4,14 +4,14 @@ function MenuChoice(){
             document.getElementById("section2").style.visibility = "hidden";
             }
         
-        else if (document.getElementById("menu").value == "Show Area 2"){
-            document.getElementById("section1").style.visibility = "hidden";
+      else if (document.getElementById("menu").value == "Show Area 2"){
+           document.getElementById("section1").style.visibility = "hidden";
             document.getElementById("section2").style.visibility = "visible";
         }
         
-        else     {
-            document.getElementById("section1").style.visibility = "hidden";
-            document.getElementById("section2").style.visibility = "hidden";
+        //else     {
+       //     document.getElementById("section1").style.visibility = "hidden";
+       //     document.getElementById("section2").style.visibility = "hidden";
         
-        }
+       // }
 }
